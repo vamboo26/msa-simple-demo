244475b4f89a8d94b711f34fbd15f8f72ed1e11c
zingoworks <zingoworks@gmail.com>
2020-08-24 14:46:32 +0900

    프로젝트 생성

---
+++ src/test/java/io/zingoworks/msasimpledemo/MsaSimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zingoworks.msasimpledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaSimpleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
