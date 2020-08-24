22edaf59e4b58b27b0420e0031f60c8808e81dee
zingoworks <zingoworks@gmail.com>
2020-08-24 16:10:03 +0900

    Github config repo와 통신 확인

--- config-server/src/test/java/io/zingoworks/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/io/zingoworks/configserver/ConfigServerApplicationTests.java
@@ -1,13 +1,13 @@
-package io.zingoworks.configserver;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ConfigServerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package io.zingoworks.configserver;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ConfigServerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
