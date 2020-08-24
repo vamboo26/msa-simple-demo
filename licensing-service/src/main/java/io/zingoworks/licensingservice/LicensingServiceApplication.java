716fa84e0fe7a5ec25e08079bff532867ea76453
zingoworks <zingoworks@gmail.com>
2020-08-24 14:58:36 +0900

    예제 프로젝트의 설정 가져오기

---
+++ licensing-service/src/main/java/io/zingoworks/licensingservice/LicensingServiceApplication.java
@@ -0,0 +1,14 @@
+package io.zingoworks.licensingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingServiceApplication {
+
+    public static void main(String[] args) {
+        //return ConfigurableApplicationContext class
+        SpringApplication.run(LicensingServiceApplication.class, args);
+    }
+
+}
